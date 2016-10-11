9cc1af42455d96da3d9cce344e343e8c73464280
Soo Park <soorin1993@gmail.com>
2016-10-10 22:01:23 -0600

    proj1 swift 2.3

---
+++ Lab6/Lab6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lab6
+//
+//  Created by Soo Rin Park on 10/6/16.
+//  Copyright © 2016 Soo Rin Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
